0c7634bc39233508c326c14ddacab13e2959ee9e
User <2085163736@qq.com>
2024-12-09 21:53:20 +0800

    update files

--- ish.cpp
+++ ish.cpp
@@ -1,8 +1,10 @@
 #include"shell.hpp"
 int main()
 {
+    ish sh;
     void show();
     show();
+    sh.PrintPrompt();
 
 
 
--- shell.hpp
+++ shell.hpp
@@ -5,3 +5,15 @@
 #include<sys/ioctl.h>
 
 using namespace std;
+
+class ish
+{
+public:
+
+    void PrintPrompt();
+
+    string hostName = "TheINK";
+    string userName = "user";
+    string wdPath = "~/CODE/ish";
+    string gitHEAD = "master";
+};
